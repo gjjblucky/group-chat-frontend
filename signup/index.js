f6afa6ff52d907f80e53c25539175388c7140d07
gjjblucky <pahadion007@gmail.com>
2022-06-11 21:41:35 +0530

    sign up frontend

---
+++ signup/index.js
@@ -0,0 +1,26 @@
+function signup(e) {
+    e.preventDefault();
+    console.log(e.target.name);
+    const form = new FormData(e.target);
+
+    const signupDetails = {
+        name: form.get("name"),
+        phoneNumber:form.get("phone no"),
+        email: form.get("email"),
+        password: form.get("password")
+
+
+    }
+    console.log(signupDetails)
+    axios.post('http://localhost:4000/user/signup',signupDetails).then(response => {
+        
+        if(response.status === 201){
+            // alert('signup successfull')
+            window.location.href = "../Login/login.html" // change the page on successful login
+        } else {
+            throw new Error('Failed to login')
+        }
+    }).catch(err => {
+        document.body.innerHTML += `<div style="color:red;">${err} <div>`;
+    });
+}
